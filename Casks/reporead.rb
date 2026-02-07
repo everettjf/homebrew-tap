83a7be54e79c9340938ac2f4908fb81277a0c84b
everettjf <everettjf@live.com>
2026-02-06 19:24:43 -0800

    bump reporead to 1.1.3

--- Casks/reporead.rb
+++ Casks/reporead.rb
@@ -1,6 +1,6 @@
 cask "reporead" do
-  version "1.1.2"
-  sha256 "73e4c97e1f98aebff087e3fdf220f45c1e8c425189d5e8c6e4307ff6085feeae"
+  version "1.1.3"
+  sha256 "e40e7663e6ead90e5cbbedc4168acd9b68ab4c88921b78105e900953cb139367"
 
   url "https://github.com/everettjf/RepoRead/releases/download/v#{version}/RepoRead.dmg"
   name "RepoRead"
