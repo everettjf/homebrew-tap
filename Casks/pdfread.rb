f07be03242e7a9924f654c85b2c3d71e77e26c05
everettjf <everettjf@live.com>
2026-02-07 02:08:18 -0800

    bump pdfread to 0.1.6

--- Casks/pdfread.rb
+++ Casks/pdfread.rb
@@ -1,6 +1,6 @@
 cask "pdfread" do
-  version "0.1.5"
-  sha256 "6269cde859143b445523dd55ee78e8d496078984974b2d9f17e1bde2260e0933"
+  version "0.1.6"
+  sha256 "85da6b01582ab29ddf3c23741e0059c1af7fc7b2f442a7b97e111835f79b173c"
 
   url "https://github.com/everettjf/PDFRead/releases/download/v#{version}/PDFRead.dmg"
   name "PDFRead"
