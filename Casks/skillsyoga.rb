fe97a64fa1138d4f6662767df5e56a103e5a5de5
everettjf <everettjf@live.com>
2026-02-17 21:54:31 -0800

    bump skillsyoga to 0.1.12

--- Casks/skillsyoga.rb
+++ Casks/skillsyoga.rb
@@ -1,6 +1,6 @@
 cask "skillsyoga" do
-  version "0.1.11"
-  sha256 "c45c896494697e0db44bacfca2a134a7317adef1010e0fc0201492d87d7ace10"
+  version "0.1.12"
+  sha256 "fc2b7d18a9bd99ced1dde82ca4263d81486e4c958c0b7c0c8e376a41308f98da"
 
   url "https://github.com/microclaw/skillsyoga/releases/download/v#{version}/SkillsYoga.dmg"
   name "SkillsYoga"
