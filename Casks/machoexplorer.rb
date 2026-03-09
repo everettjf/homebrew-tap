1fa88efdfe62ba94f8fd8010f71cf52f7c87b0f6
everettjf <everettjf@live.com>
2026-03-08 19:11:37 -0700

    bump machoexplorer to 2.0.0

---
+++ Casks/machoexplorer.rb
@@ -0,0 +1,11 @@
+cask "machoexplorer" do
+  version "2.0.0"
+  sha256 "0a1afe3b258cf51a497579ef29867f65ed835d43f192ebd2c8f43f1715dce901"
+
+  url "https://github.com/everettjf/MachOExplorer/releases/download/v2.0.0/MachOExplorer.dmg"
+  name "MachOExplorer"
+  desc "Mach-O and archive explorer"
+  homepage "https://github.com/everettjf/MachOExplorer"
+
+  app "MachOExplorer.app"
+end
