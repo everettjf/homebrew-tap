f3d647cfd0dce00e945e942cde418025b0c7f47e
everettjf <everettjf@live.com>
2026-03-12 23:22:11 -0700

    bump machoexplorer to 2.0.13

--- Casks/machoexplorer.rb
+++ Casks/machoexplorer.rb
@@ -1,8 +1,8 @@
 cask "machoexplorer" do
-  version "2.0.12"
-  sha256 "d79daa94abac0a1af9b75c1b9b98e07138a5a73faee689c89a6bda93bfb028ce"
+  version "2.0.13"
+  sha256 "15242b1a517b88a3192eb69925711b8216edadd24317042909aa64b3e8f475a4"
 
-  url "https://github.com/everettjf/MachOExplorer/releases/download/v2.0.12/MachOExplorer.dmg"
+  url "https://github.com/everettjf/MachOExplorer/releases/download/v2.0.13/MachOExplorer.dmg"
   name "MachOExplorer"
   desc "Mach-O and archive explorer"
   homepage "https://github.com/everettjf/MachOExplorer"
