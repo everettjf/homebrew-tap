a50db4ea582160eab08bcaa9324fcdeb1d0c470b
everettjf <everettjf@live.com>
2026-03-17 21:39:06 -0700

    bump typemore to 0.1.6

--- Casks/typemore.rb
+++ Casks/typemore.rb
@@ -1,8 +1,8 @@
 cask "typemore" do
-  version "0.1.4"
-  sha256 "38e2542061d2107c5edafcb31357a94f3e016204f95eb69c1a3dbd6da64809bb"
+  version "0.1.6"
+  sha256 "1458bd29f6c5b0d875470d37132d6a0fbd705b456489103fd548006fbda383cd"
 
-  url "https://github.com/everettjf/typemore/releases/download/v0.1.4/TypeMore.dmg"
+  url "https://github.com/everettjf/typemore/releases/download/v0.1.6/TypeMore.dmg"
   name "TypeMore"
   desc "Offline speech recognition desktop app built with Tauri"
   homepage "https://github.com/everettjf/typemore"
