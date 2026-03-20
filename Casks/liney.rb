d2ed41184bbac5a941ce560e64dcbc0589de9cff
everettjf <everettjf@live.com>
2026-03-20 09:41:33 -0700

    refresh liney 1.0.0 notarized sha

--- Casks/liney.rb
+++ Casks/liney.rb
@@ -1,6 +1,6 @@
 cask "liney" do
   version "1.0.0"
-  sha256 "5ac386d03c5c62a010b6800a3936dacc9f4240c24ca41020b3c372e4aa8b7495"
+  sha256 "9360f8dd6314f5e48e06ac199a54045d257364afc25b58ce3687b53a7a19cabe"
 
   url "https://github.com/everettjf/liney/releases/download/v#{version}/Liney-#{version}.dmg"
   name "Liney"
