daacb4cd47f61e08041ff3340b54e5710df507a2
Everett <everettjf@live.com>
2026-08-01 19:45:42 -0700

    bump liney to 1.0.79

--- Casks/liney.rb
+++ Casks/liney.rb
@@ -1,6 +1,6 @@
 cask "liney" do
-  version "1.0.78"
-  sha256 "32d3b9f56b8b2cd435a55d3ccdaa33134594b798d4f6a8cda2e9665dedc1c408"
+  version "1.0.79"
+  sha256 "6069a9518889e25b2ea1db60fe41c9a6e00191122567a3b548496d1555a54adc"
 
   url "https://github.com/everettjf/liney/releases/download/v#{version}/Liney-#{version}.dmg"
   name "Liney"
