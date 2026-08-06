cd63e4a3f664c7af37d524930a0be5a7e054ac4a
Everett <everettjf@live.com>
2026-08-05 19:06:19 -0700

    Update EasyVM to 3.0.0

--- Casks/easyvm.rb
+++ Casks/easyvm.rb
@@ -1,13 +1,23 @@
+# typed: strict
+# frozen_string_literal: true
+
 cask "easyvm" do
-  version "1.1.0"
-  sha256 "18ee5df56c3aff59debd40d97de4221aec81e39f5d7792f5cf5096fd610cd6cc"
+  version "3.0.0"
+  sha256 "7bbd5d66d25461ca1d1e9c0fdd9629c3d150872b2f28997e69bdbf48b3088fef"
 
-  url "https://github.com/everettjf/EasyVM/releases/download/v1.1.0/EasyVM.dmg"
+  url "https://github.com/everettjf/easyvm/releases/download/v#{version}/EasyVM-#{version}.zip"
   name "EasyVM"
-  desc "Lightweight virtual machine app for macOS"
-  homepage "https://github.com/everettjf/EasyVM"
+  desc "Simple native virtual machines for Apple silicon Macs"
+  homepage "https://everettjf.github.io/easyvm/"
 
-  depends_on macos: ">= :ventura"
+  depends_on arch: :arm64
+  depends_on macos: :ventura
 
   app "EasyVM.app"
+
+  zap trash: [
+    "~/Library/Application Support/EasyVM",
+    "~/Library/Preferences/com.everettjf.easyvm.plist",
+    "~/Library/Saved Application State/com.everettjf.easyvm.savedState",
+  ]
 end
