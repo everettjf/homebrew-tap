783ce01a7df64ceff0ff6194c26dd46b19c6c8b5
Everett <everettjf@live.com>
2026-08-21 00:25:03 -0700

    Avoid cached EasyVM 3.2.7 archive

--- Casks/easyvm.rb
+++ Casks/easyvm.rb
@@ -5,7 +5,7 @@ cask "easyvm" do
   version "3.2.7"
   sha256 "fdf027de84414f1ef2f1c57e72ddb85201bede0754375a60aaa8b6ea7bcbdd99"
 
-  url "https://github.com/everettjf/easyvm/releases/download/v#{version}/EasyVM-#{version}.zip"
+  url "https://github.com/everettjf/easyvm/releases/download/v#{version}/EasyVM-#{version}.zip?stapled=1"
   name "EasyVM"
   desc "Simple native virtual machines for Apple silicon Macs"
   homepage "https://xnu.app/easyvm"
