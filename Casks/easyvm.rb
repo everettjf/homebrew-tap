3849a839b01ee456da966db3207b6d56f186da9d
Everett <everettjf@live.com>
2026-08-21 22:59:46 -0700

    Update EasyVM to 3.2.21

--- Casks/easyvm.rb
+++ Casks/easyvm.rb
@@ -2,8 +2,8 @@
 # frozen_string_literal: true
 
 cask "easyvm" do
-  version "3.2.20"
-  sha256 "d351ec27bf5fe44c114dc16affd1e0141d339878219f4f152e4f1ac665faba1a"
+  version "3.2.21"
+  sha256 "8ff2f8df7209c8feca8b3a2447ebee8700961bd43c5daac773768f0823ca4d1d"
 
   url "https://github.com/everettjf/easyvm/releases/download/v#{version}/EasyVM-#{version}.zip?notarized=1"
   name "EasyVM"
