edd175eb4767abda920cf3ff0655dcddae1ec919
Everett <everettjf@live.com>
2026-08-22 23:07:33 -0700

    Add LaunchDeck cask

---
+++ Casks/launchdeck.rb
@@ -0,0 +1,20 @@
+cask "launchdeck" do
+  version "1.5.0"
+  sha256 "101c75f307e12b3690e9c492e6680b577d0dbcab58002501698a975134dd8930"
+
+  url "https://github.com/everettjf/launchdeck/releases/download/v#{version}/LaunchDeck-#{version}.zip"
+  name "LaunchDeck"
+  desc "Private local-first application launcher"
+  homepage "https://xnu.app/launchdeck/"
+
+  depends_on macos: :sequoia
+
+  app "LaunchDeck.app"
+
+  zap trash: [
+    "~/Library/Application Support/LaunchDeck",
+    "~/Library/Caches/com.everettjf.launchdeck",
+    "~/Library/Preferences/com.everettjf.launchdeck.plist",
+    "~/Library/Saved Application State/com.everettjf.launchdeck.savedState",
+  ]
+end
