75af60ca4671c0a4bd695cb18feb415dda0af52b
Everett <everettjf@live.com>
2026-08-25 22:52:10 -0700

    Update LaunchDeck to 2.0.8

--- Casks/launchdeck.rb
+++ Casks/launchdeck.rb
@@ -1,6 +1,6 @@
 cask "launchdeck" do
-  version "2.0.7"
-  sha256 "447689ce4c85ab32d12dda31eaf4d63a61c9efe2a67e3c842f358774abb599cc"
+  version "2.0.8"
+  sha256 "54b8ac9e12d49b979b32b87540737f2561da2e306b3898a845bf17d6b4bbcace"
 
   url "https://github.com/everettjf/launchdeck/releases/download/v#{version}/LaunchDeck-#{version}.zip"
   name "LaunchDeck"
