815df059a763f96bb7ccde824e4c2b7a2e525efa
Everett <everettjf@live.com>
2026-08-27 14:33:06 -0700

    Update EZVM to 5.0.1

--- Casks/ezvm.rb
+++ Casks/ezvm.rb
@@ -2,8 +2,8 @@
 # frozen_string_literal: true
 
 cask "ezvm" do
-  version "5.0.0"
-  sha256 "1cc7a8db93e005cffe471c8abc0b910820b2d9bb3cae4676e2eaad603de5abf3"
+  version "5.0.1"
+  sha256 "15fa973c51db954cd241d71ef0e56a0b0a07a1d7b9ea3406cc339d3c46303dc3"
 
   url "https://github.com/everettjf/ezvm/releases/download/v#{version}/EZVM-#{version}.zip?notarized=1"
   name "EZVM"
