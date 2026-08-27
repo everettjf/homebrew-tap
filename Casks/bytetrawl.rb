49ad2d180ec577f91771db774580fdfa13ac078c
Everett <everettjf@live.com>
2026-08-26 20:12:13 -0700

    Fix ByteTrawl public release URLs

--- Casks/bytetrawl.rb
+++ Casks/bytetrawl.rb
@@ -2,7 +2,7 @@ cask "bytetrawl" do
   version "1.0.0"
   sha256 "eb87c93be7cdcb7dd1e9b5a8b4d784e208ab8be3c2bb7cda018acb9314a0bd6b"
 
-  url "https://github.com/everettjf/bytetrawl/releases/download/v#{version}/ByteTrawl-#{version}-macos.zip"
+  url "https://github.com/everettjf/homebrew-tap/releases/download/bytetrawl-v#{version}/ByteTrawl-#{version}-macos.zip"
   name "ByteTrawl"
   desc "Application, package, and binary inspection workbench"
   homepage "https://github.com/everettjf/bytetrawl"
--- Formula/bytetrawl-cli.rb
+++ Formula/bytetrawl-cli.rb
@@ -1,7 +1,7 @@
 class BytetrawlCli < Formula
   desc "Static application, package, and binary inspection workbench"
   homepage "https://github.com/everettjf/bytetrawl"
-  url "https://github.com/everettjf/bytetrawl/releases/download/v1.0.0/bytetrawl-cli-1.0.0-aarch64-apple-darwin.tar.gz"
+  url "https://github.com/everettjf/homebrew-tap/releases/download/bytetrawl-v1.0.0/bytetrawl-cli-1.0.0-aarch64-apple-darwin.tar.gz"
   sha256 "ad2a485514841c5b0e2a7007ba40633663ee3f342a6080b4988bf609095df7d9"
   license "Apache-2.0"
 
