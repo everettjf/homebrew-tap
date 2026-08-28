24e771c8c420dd2d27882822a27476dfbdc91cb6
Everett <everettjf@live.com>
2026-08-28 12:20:34 -0700

    Update EZVM to 1.0.0

--- Casks/ezvm.rb
+++ Casks/ezvm.rb
@@ -3,7 +3,7 @@
 
 cask "ezvm" do
   version "1.0.0"
-  sha256 "3933a1f28327035338b1892ca2fde19d88836ad5fc7093147683f04c1f5d5bb9"
+  sha256 "2bbfad666c43cc039ffda43ad1caf6fa67f94c6c4df76f055b8381b1c8aa69dc"
 
   url "https://github.com/everettjf/ezvm/releases/download/v#{version}/EZVM-#{version}.zip?notarized=1"
   name "EZVM"
