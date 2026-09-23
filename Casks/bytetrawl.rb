3e535beca1367f7263bb95bcfc58a6bc08b6fe2b
Everett <everettjf@live.com>
2026-09-23 08:01:32 -0700

    Release ByteTrawl 1.1.16

--- Casks/bytetrawl.rb
+++ Casks/bytetrawl.rb
@@ -1,6 +1,6 @@
 cask "bytetrawl" do
-  version "1.1.15"
-  sha256 "ee31c808de3ed4ec19cd72041941748480f6e6712128b204464b4b6dd41be88b"
+  version "1.1.16"
+  sha256 "ee6fd972b9db9a1d854dce00a0bb6dba9eb6668ed6147290bf08e685f28c7ef5"
 
   url "https://github.com/everettjf/homebrew-tap/releases/download/bytetrawl-v#{version}/ByteTrawl-#{version}-macos.zip"
   name "ByteTrawl"
--- Formula/bytetrawl-cli.rb
+++ Formula/bytetrawl-cli.rb
@@ -1,8 +1,8 @@
 class BytetrawlCli < Formula
   desc "Static application, package, and binary inspection workbench"
   homepage "https://github.com/everettjf/bytetrawl"
-  url "https://github.com/everettjf/homebrew-tap/releases/download/bytetrawl-v1.1.15/bytetrawl-cli-1.1.15-aarch64-apple-darwin.tar.gz"
-  sha256 "77b18ed8a917cb7dda33d09da9d0f5c79d5a759662b1b6b12d42805bbc944152"
+  url "https://github.com/everettjf/homebrew-tap/releases/download/bytetrawl-v1.1.16/bytetrawl-cli-1.1.16-aarch64-apple-darwin.tar.gz"
+  sha256 "822d8bc6ba52003873bdeae906903f01af01cad00b85c1561130c016af285de7"
   license "Apache-2.0"
 
   depends_on arch: :arm64
